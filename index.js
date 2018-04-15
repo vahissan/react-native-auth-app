67546138afa4173e501e30d24100d19346fa2eab
Vahissan Nandakumar <email@vahissan.com>
2018-04-14 20:35:01 -0400

    ignore known warnings after upgrading to relatively new react-native version 0.55

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import './app/config/Reactotron';
 
-import { AppRegistry } from 'react-native';
+import { AppRegistry, YellowBox } from 'react-native';
 import App from './app/App';
+
+YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
 
 AppRegistry.registerComponent('ReactNativeAuthApp', () => App);
